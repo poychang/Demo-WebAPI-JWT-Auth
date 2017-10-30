f5a012398d5087313aa37f0a9e57a9874bd312b8
Poy Chang <poy.chang@seec.com.tw>
2017-10-30 11:21:12 +0800

    新增 Jwt 控制器，用於使用者登入後回傳 JWT 相關資訊，並設定成 AllowAnonymous

---
+++ DemoWebApiJwtAuth/Controllers/JwtController.cs
@@ -0,0 +1,26 @@
+﻿using DemoWebApiJwtAuth.Models;
+using DemoWebApiJwtAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoWebApiJwtAuth.Controllers
+{
+    [Route("api/[controller]")]
+    public class JwtController : Controller
+    {
+        // POST: api/jwt
+        /// <summary>
+        /// 登入取得並取得 JWT
+        /// </summary>
+        /// <param name="logingUser">使用者帳號資訊</param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post([FromBody]LoginModel logingUser)
+        {
+            return new EmptyResult();
+        }
+    }
+}
