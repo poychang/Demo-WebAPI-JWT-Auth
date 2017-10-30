5b57d7431c00113ea35cfe718f845388c8795c0c
Poy Chang <poy.chang@seec.com.tw>
2017-10-30 11:32:01 +0800

    建立驗證使用者的方法（DEMO 用，這邊請改成符合您環境的驗證方式）

--- DemoWebApiJwtAuth/Controllers/JwtController.cs
+++ DemoWebApiJwtAuth/Controllers/JwtController.cs
@@ -1,4 +1,6 @@
-﻿using DemoWebApiJwtAuth.Models;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
 using DemoWebApiJwtAuth.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,9 +20,48 @@ namespace DemoWebApiJwtAuth.Controllers
         /// <returns></returns>
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult Post([FromBody]LoginModel logingUser)
+        public async Task<IActionResult> Post([FromBody]LoginModel logingUser)
         {
+            // 驗證並取得使用者資訊
+            var identity = await GetClaimsIdentity(logingUser.Username, logingUser.Password);
+            if (identity == null)
+            {
+                return BadRequest("Invalid credentials");
+            }
             return new EmptyResult();
+        }
+
+        /// <summary>
+        /// 取得使用者識別資訊
+        /// </summary>
+        /// <remarks>DEMO 用，這邊請改成符合您環境的驗證方式</remarks>
+        /// <param name="username">帳號</param>
+        /// <param name="password">密碼</param>
+        /// <returns></returns>
+        private static Task<ClaimsIdentity> GetClaimsIdentity(string username, string password)
+        {
+            if (username == "admin" &&
+                password == "password")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"),
+                    new[]
+                    {
+                        new Claim("Role", "Admin")
+                    }));
+            }
+
+            if (username == "member" &&
+                password == "password")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"),
+                    new[]
+                    {
+                        new Claim("Role", "Member")
+                    }));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return Task.FromResult<ClaimsIdentity>(null);
         }
     }
 }
