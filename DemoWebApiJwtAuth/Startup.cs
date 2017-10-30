3f73fa101018a395c2418dfc102b87cca4579dfe
Poy Chang <poy.chang@seec.com.tw>
2017-10-30 11:11:44 +0800

    設定全面驗證路由

--- DemoWebApiJwtAuth/Startup.cs
+++ DemoWebApiJwtAuth/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,7 +25,17 @@ namespace DemoWebApiJwtAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            // 註冊 Options Pattern 服務，將配置內容註冊到容器裡，來獲取對應的服務 Provider 對象
+            services.AddOptions();
+
+            // 設定全面驗證 MVC 路由
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +45,9 @@ namespace DemoWebApiJwtAuth
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // 負責自動驗證權限的 Middleware
+            app.UseAuthentication();
 
             app.UseMvc();
         }
