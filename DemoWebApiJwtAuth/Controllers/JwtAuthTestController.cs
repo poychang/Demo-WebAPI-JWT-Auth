80d496ac93fb83c1dee742b5d86299969a297492
Poy Chang <poy.chang@seec.com.tw>
2017-10-30 11:36:16 +0800

    新增 JwtAuthTest 控制器，用於測試 JWT 的使用

---
+++ DemoWebApiJwtAuth/Controllers/JwtAuthTestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoWebApiJwtAuth.Controllers
+{
+    [Route("api/[controller]")]
+    public class JwtAuthTestController : Controller
+    {
+        // GET: api/JwtAuthTest/Admin
+        [HttpGet("admin")]
+        [Authorize(Policy = "Admin")]
+        public IActionResult GetAdmin()
+        {
+            return new JsonResult("I Am Admin");
+        }
+
+        // GET: api/JwtAuthTest/Member
+        [HttpGet("member")]
+        [Authorize(Policy = "Member")]
+        public IActionResult GetMember()
+        {
+            return new JsonResult("I Am Member");
+        }
+    }
+}
