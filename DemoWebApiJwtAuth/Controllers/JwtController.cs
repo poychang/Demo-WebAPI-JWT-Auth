f55eafb27be1db1040a24049fda42a28c78e2b2e
Poy Chang <poy.chang@seec.com.tw>
2017-10-30 11:34:39 +0800

    注入 JwtIssuerOptions，並回傳編碼後的 JWT 資訊

--- DemoWebApiJwtAuth/Controllers/JwtController.cs
+++ DemoWebApiJwtAuth/Controllers/JwtController.cs
@@ -1,9 +1,13 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using DemoWebApiJwtAuth.Models;
+using DemoWebApiJwtAuth.Options;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +16,20 @@ namespace DemoWebApiJwtAuth.Controllers
     [Route("api/[controller]")]
     public class JwtController : Controller
     {
+        /// <summary>
+        /// JWT 設定項
+        /// </summary>
+        private readonly JwtIssuerOptions _jwtIssuerOptions;
+
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        /// <param name="jwtOptions"></param>
+        public JwtController(IOptions<JwtIssuerOptions> jwtOptions)
+        {
+            _jwtIssuerOptions = jwtOptions.Value;
+        }
+
         // POST: api/jwt
         /// <summary>
         /// 登入取得並取得 JWT
@@ -28,8 +46,38 @@ namespace DemoWebApiJwtAuth.Controllers
             {
                 return BadRequest("Invalid credentials");
             }
-            return new EmptyResult();
+
+            // 產生 JWT 並進行編碼
+            var jwt = new JwtSecurityToken(
+                issuer: _jwtIssuerOptions.Issuer,
+                audience: _jwtIssuerOptions.Audience,
+                claims: new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, logingUser.Username),
+                    new Claim(JwtRegisteredClaimNames.Jti, await _jwtIssuerOptions.JtiGenerator()),
+                    new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(_jwtIssuerOptions.IssuedAt).ToString(), ClaimValueTypes.Integer64),
+                    identity.FindFirst("Role")
+                },
+                notBefore: _jwtIssuerOptions.NotBefore,
+                expires: _jwtIssuerOptions.Expiration,
+                signingCredentials: _jwtIssuerOptions.SigningCredentials);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                accessToken = encodedJwt,
+                expiresIn = (int)_jwtIssuerOptions.ValidFor.TotalSeconds
+            };
+            return new JsonResult(response);
         }
+
+        /// <summary>
+        /// 計算距離 UNIX 紀元時間的秒數
+        /// </summary>
+        /// <param name="date">日期</param>
+        /// <returns>秒數</returns>
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 
         /// <summary>
         /// 取得使用者識別資訊
